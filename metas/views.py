6b5a3d823c70fb2dbe9537406700d3e01345f712
Javier Sanchez Toledano <js.toledano@me.com>
2021-02-03 13:56:39 -0600

    Adding todo's

--- metas/views.py
+++ metas/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "index.html"
+
+# TODO: Create the base template
+# TODO: Create a form class to show a form based in json field.
+# TODO: Create a page to show a proof record using json field.
